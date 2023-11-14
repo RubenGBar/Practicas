4aeab2f06d4766860aae60edff6b8ccb18747bc0
ruben <ruben@DESKTOP-QTJ799Q>
2023-11-14 20:02:37 +0100

    Cuadrado Cruz arreglado

--- Practicas/src/figuras/CuadradoCruz.java
+++ Practicas/src/figuras/CuadradoCruz.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class CuadradoCruz {
 	public static void main(String[] args) {
-		int num;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Introduzca un numero");
-		
-		num = sc.nextInt();
-		
-		for (int i = 0; i < num; i++) {
-			for (int j = 0; j < num; j++) {
-				if (i == 0 || j == 0 || i == num - 1 || j == num - 1 || j == (num / 2) || i == (num - i ) - 1) {
-					System.out.print("* ");
-				}else {
-					System.out.print("  ");
-				}
-			}
-			System.out.println();
-		}
-		
-		sc.close();
-	}
+        int num;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca un numero");
+
+        num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                if (i == 0 || j == 0 || i == num - 1 || j == num - 1 || j == num / 2 || i == num / 2) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
